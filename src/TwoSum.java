fe454b59274c898f40832f2ed09b5d8b2b3da546
NikhilKumarNK5 <nikhil.nikki.nk05@gmail.com>
2025-07-14 01:05:39 +0530

    TwoSum

--- src/TwoSum.java
+++ src/TwoSum.java
@@ -24,8 +24,82 @@ Constraints:
 Only one valid answer exists.
 */
 
-public class TwoSum {
-    public int[] twoSum(int[] nums, int target) {
+import java.util.Arrays;
+import java.util.HashMap;
 
+public class TwoSum {
+
+    // Approach 1: Brute Force Approach
+    // for each element in nums check all the other elements and verify if the sum is equal to the target of not
+    // if the sum equals target that store the element indices in the result array and return
+    // TC => O(N^2)
+    public int[] twoSum(int[] nums, int target) {
+        int[] res = new int[2];
+        for(int i = 0; i < nums.length; i++) {
+            for(int j = i + 1; j < nums.length; j++) {
+                if(nums[i] + nums[j] == target) {
+                    res[0] = i;
+                    res[1] = j;
+                }
+            }
+        }
+        return res;
     }
+
+    // Approach 2: Using Sorting
+    // We can sort the array and store in new variable (O(nlogn))
+    // We can do the binary search and find out if an element, and it's target - element value exists or not (O(logn))
+    // We can get the elements and in the original array we can search for the indices of the those elements and return the indices as required
+    // TC => O(NlogN)
+    // SC => O(N)
+    public int[] twoSumSortAndSearch(int[] nums, int target) {
+        // copy and sort the array to preserve the original array
+        int[] sorted = Arrays.copyOf(nums, nums.length);
+        Arrays.sort(sorted);
+
+        // using binary search to find out the respective numbers
+        int num1 = -1, num2 = -1;
+        for (int i = 0; i < sorted.length; i++) {
+            int difference = target - sorted[i];
+            int index = Arrays.binarySearch(sorted, i + 1, sorted.length, difference);
+            if (index > i) {
+                num1 = sorted[i];
+                num2 = sorted[index];
+                break;
+            }
+        }
+
+        // finding original indices in the unsorted array
+        int index1 = -1, index2 = -1;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == num1 && index1 == -1) {
+                index1 = i;
+            } else if (nums[i] == num2 && index2 == -1) {
+                index2 = i;
+            }
+        }
+
+        return new int[] {index1, index2};
+    }
+
+    // Approach 3: Using HashMap
+    // Use the element as key and index as value
+    // Will check if the value and the difference exists in the HashMap
+    // if exists we can get their indices otherwise we can move forward and add the element and index in HashMap
+    // TC => O(N)
+    // SC => O(N)
+    public int[] twoSumHashMap(int[] nums, int target) {
+        HashMap<Integer, Integer> hs = new HashMap<>();
+
+        for(int i = 0; i < nums.length; i++) {
+            int difference = target - nums[i];
+            if(hs.containsKey(difference)) {
+                return new int[] {hs.get(difference), i};
+            } else {
+                hs.put(nums[i], i);
+            }
+        }
+        return null;
+    }
+
 }
