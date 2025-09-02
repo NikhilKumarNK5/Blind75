73e6d318aba557b3c30a208f47b232399ec6e46b
NikhilKumarNK5 <nikhil.nikki.nk05@gmail.com>
2025-09-02 12:26:13 +0530

    ReverseLinkedList

--- src/ReverseLinkedList.java
+++ src/ReverseLinkedList.java
@@ -97,7 +97,7 @@ public class ReverseLinkedList {
     public ListNode reverseListRecursive(ListNode head) {
         if(head == null || head.next == null)
             return head;
-        ListNode newHead = reverseList(head.next);
+        ListNode newHead = reverseListRecursive(head.next);
         head.next.next = head;
         head.next = null;
         return newHead;
